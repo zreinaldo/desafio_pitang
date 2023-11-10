a263bd55164ccb6edbefb29d62ca733a664e1fc4
Reinaldo Oliveira Filho <dankada@gmail.com>
2023-11-10 17:38:45 -0300

    fix(UserRepository): correçao alterar

--- desafio-crud/src/main/java/br/com/desafio/desafiocrud/service/UserService.java
+++ desafio-crud/src/main/java/br/com/desafio/desafiocrud/service/UserService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.desafio.desafiocrud.model.User;
 import br.com.desafio.desafiocrud.repository.UserRepository;
@@ -13,29 +12,23 @@ import br.com.desafio.desafiocrud.repository.UserRepository;
 @Service
 public class UserService {
 
-@Autowired
-private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
+    public List<User> findUsers() {
 
-    public List<User> findUsers(){
         return userRepository.findAll();
     }
-    
-    public Optional<User> findUser(Integer idUser){
-        return  userRepository.findById(idUser);
-    }
 
-    public Optional<User>  getUse(@PathVariable Integer idUser){
+    public Optional<User> findUser(Integer idUser) {       
         return userRepository.findById(idUser);
     }
-
-
+    
     public User saveUser(User user) {
-        return  userRepository.save(user);
+        return userRepository.save(user);
     }
 
-    
-    public void deleteUser(Integer idUser)  {
+    public void deleteUser(Integer idUser) {
         userRepository.deleteById(idUser);
     }
 
